21d4c45badd4a215ca7619f1a48c6c17e6182bfb
lgilbert <lgilbert@ITLC3ML593.jhbdvt.co.za>
2021-04-13 16:16:50 +0200

    This is our first commit to the Master branch of this project

---
+++ firstMavenisedProject/src/main/java/com/org/firstMavenisedProject/NewClass01.java
@@ -0,0 +1,11 @@
+package com.org.firstMavenisedProject;
+
+public class NewClass01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi team");
+
+	}
+
+}
