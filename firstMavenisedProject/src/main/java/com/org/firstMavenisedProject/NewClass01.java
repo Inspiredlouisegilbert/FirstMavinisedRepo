1e226f3f371863d8d585c97b5dc35bc3bef736b6
lgilbert <lgilbert@ITLC3ML593.jhbdvt.co.za>
2021-04-13 16:52:52 +0200

    Added some basic selenium to the pom

--- firstMavenisedProject/src/main/java/com/org/firstMavenisedProject/NewClass01.java
+++ firstMavenisedProject/src/main/java/com/org/firstMavenisedProject/NewClass01.java
@@ -1,10 +1,17 @@
 package com.org.firstMavenisedProject;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class NewClass01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("New Banch");
+		// declaration and instatiation of the obeject and variables
+		System.setProperty("webdriver.chrome.driver", "c:\\chromedriver_win32\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
 
 	}
 
